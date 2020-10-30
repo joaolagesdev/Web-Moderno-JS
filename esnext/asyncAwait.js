5d1922fd93d967cceade213d804365eb1a609524
João Artur <67400301+joaothomaz@users.noreply.github.com>
2020-10-30 20:50:47 -0300

    chore: how to use async/await

---
+++ esnext/asyncAwait.js
@@ -0,0 +1,33 @@
+const http = require('http')
+
+const getTurma = (letra, callback) => {
+    const url = `http://files.cod3r.com.br/curso-js/turma${letra}.json`
+    return new Promise((resolve, reject) => {
+        http.get(url, res => {
+            let resultado = ''
+            
+            res.on('data', dados => {
+                resultado += dados
+            })
+    
+            res.on('end', () => {
+                try {
+                    resolve(JSON.parse(resultado))
+                } catch(e) {
+                    reject(e)
+                }
+            })
+        })
+    })
+}
+
+// Objetivo de simplificar o uso de promisses (ES8)
+let obterAlunos = async () => {
+    const ta = await getTurma('A')
+    const tb = await getTurma('B')
+    return [].concat(ta, tb) // gerar um único array
+} // retorna um objeto AsyncFunction
+
+obterAlunos()
+    .then(alunos => alunos.map(a => a.nome))
+    .then(nomes => console.log(nomes))
